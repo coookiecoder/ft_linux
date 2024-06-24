53fa204b83952cf67561e214d119bfa2fc5ee669
Ambroise Bareux <ambroise.bareux@gmail.com>
2024-06-24 18:18:19 +0200

    mkdir patch setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 
 wget --input-file=wget-list-sysv --continue --directory-prefix=/mnt/lfs/sources
-mkdir -pv /mnt/lfs/{etc,var} /mnt/lfs/usr/{bin,lib,sbin}
+mkdir -pv /mnt/lfs/usr /mnt/lfs/var /mnt/lfs/usr/bin /mnt/lfs/usr/lib /mnt/lfs/usr/sbin
 groupadd lfs
 useradd -s /bin/bash -g lfs -m -k /dev/null lfs
 chown -v lfs /mnt/lfs/{usr{,/*},lib,var,etc,bin,sbin,tools}
