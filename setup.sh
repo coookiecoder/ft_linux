8fbf70a3396f870332e84f290220b8be442f7e16
Ambroise Bareux <ambroise.bareux@gmail.com>
2024-06-24 18:20:09 +0200

    chown patch setup

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ wget --input-file=wget-list-sysv --continue --directory-prefix=/mnt/lfs/sources
 mkdir -pv /mnt/lfs/usr /mnt/lfs/var /mnt/lfs/usr/bin /mnt/lfs/usr/lib /mnt/lfs/usr/sbin
 groupadd lfs
 useradd -s /bin/bash -g lfs -m -k /dev/null lfs
-chown -v lfs /mnt/lfs/{usr{,/*},lib,var,etc,bin,sbin,tools}
+chown -v lfs /mnt/lfs/usr /mnt/lfs/var /mnt/lfs/usr/bin /mnt/lfs/usr/lib /mnt/lfs/usr/sbin
