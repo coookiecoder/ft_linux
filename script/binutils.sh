e4fa132aa9649ad77351dad1c4f68e00e77c87d4
Ambroise Bareux <ambroise.bareux@gmail.com>
2024-06-25 00:00:25 +0200

    build call under script

---
+++ script/binutils.sh
@@ -0,0 +1,6 @@
+tar -xf binutils-2.42.tar.xz
+mkdir -v binutils-2.42/build
+cd binutils-2.42/build
+../configure --prefix=$LFS/tools --with-sysroot=$LFS --target=x86_64-lfs-linux-gnu --disable-nls  --enable-gprofng=no --disable-werror --enable-default-hash-style=gnu
+make
+make install
