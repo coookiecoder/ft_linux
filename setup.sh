33355f3ee5fc4293bc5d9e30db01e4cd33c7ec83
Ambroise Bareux <ambroise.bareux@gmail.com>
2024-06-24 18:23:39 +0200

    chown patch setup

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ mkdir -pv /mnt/lfs/usr /mnt/lfs/var /mnt/lfs/usr/bin /mnt/lfs/usr/lib /mnt/lfs/u
 groupadd lfs
 useradd -s /bin/bash -g lfs -m -k /dev/null lfs
 chown -v lfs /mnt/lfs/usr /mnt/lfs/var /mnt/lfs/usr/bin /mnt/lfs/usr/lib /mnt/lfs/usr/sbin
+chown -v lfs /mnt/lfs/sources /mnt/lfs/sources/*
