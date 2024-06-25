826436e9a650bf8d796d785fe46ccecf96b809dd
Ambroise Bareux <ambroise.bareux@gmail.com>
2024-06-25 02:42:25 +0200

    added api_header in build file

---
+++ script/API_header.sh
@@ -0,0 +1,12 @@
+LFS=/mnt/lfs
+
+cd /mnt/lfs/sources
+tar -xf linux-6.7.4.tar.xz
+cd linux-6.7.4
+
+make mrproper
+
+make headers
+find usr/include -name '.*' -delete
+rm usr/include/Makefile
+cp -rv usr/include $LFS/usr
