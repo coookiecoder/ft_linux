2b496079d37d19103d97a48870699434fa2cf1f4
Ambroise Bareux <ambroise.bareux@gmail.com>
2024-06-25 02:32:26 +0200

    tried patching the path

--- script/gcc.sh
+++ script/gcc.sh
@@ -31,4 +31,4 @@ make
 make install
 
 cd ..
-cat gcc/limitx.h gcc/glimits.h gcc/limity.h > `dirname $($LFS_TGT-gcc -print-libgcc-file-name)`/install-tools/include/limits.h
+cat gcc/limitx.h gcc/glimits.h gcc/limity.h > "$LFS_TGT-gcc"/install-tools/include/limits.h
