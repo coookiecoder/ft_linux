1a94fe1fa4078060503c0d6c7e1e619dfcda4fcb
Ambroise Bareux <ambroise.bareux@gmail.com>
2024-06-25 03:26:50 +0200

    put full path for libc

--- test_libc.sh
+++ test_libc.sh
@@ -1,9 +1,9 @@
 LFS_TGT=$(uname -m)-lfs-linux-gnu
 
-cd /mnt/lfs/sources/glibc-2.39/build
+cd /mnt/lfs/
 
 echo 'int main(){}' > dummy.c
-$LFS_TGT-gcc dummy.c -o a.out
+/mnt/lfs/tools/bin/$LFS_TGT-gcc dummy.c -o a.out
 readelf -l a.out | grep '/ld-linux'
 
 rm -v dummy.c a.out
