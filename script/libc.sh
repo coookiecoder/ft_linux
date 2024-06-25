8e36b98fbcc426dc55bd7df4534f188ab2c72ef5
Ambroise Bareux <ambroise.bareux@gmail.com>
2024-06-25 05:22:38 +0200

    fml wrong lfs version

--- script/libc.sh
+++ script/libc.sh
@@ -13,12 +13,21 @@ case $(uname -m) in
     ;;
 esac
 
-patch -Np1 -i ../glibc-2.33-fhs-1.patch
+patch -Np1 -i ../glibc-2.39-fhs-1.patch
 
 mkdir -v build
 cd       build
+`
+echo "rootsbindir=/usr/sbin" > configparms
 
-../configure --prefix=/usr --host=$LFS_TGT --build=$(../scripts/config.guess) --enable-kernel=3.2 --with-headers=$LFS/usr/include libc_cv_slibdir=/lib
+../configure                             \
+      --prefix=/usr                      \
+      --host=$LFS_TGT                    \
+      --build=$(../scripts/config.guess) \
+      --enable-kernel=4.19               \
+      --with-headers=$LFS/usr/include    \
+      --disable-nscd                     \
+      libc_cv_slibdir=/usr/lib
 
 make
 make DESTDIR=$LFS install
