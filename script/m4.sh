73b3a71eb157b5972a00cb3a6fe28c52bf58aaef
Ambroise <ambroise.bareux@gmail.com>
2024-06-25 07:49:56 -0500

    added m4 compilation

---
+++ script/m4.sh
@@ -0,0 +1,13 @@
+LFS=/mnt/lfs
+LFS_TGT=$(uname -m)-lfs-linux-gnu
+
+cd /mnt/lfs/sources
+tar -xf m4-1.4.19.tar.xz
+cd m4-1.4.19
+
+./configure --prefix=/usr   \
+            --host=$LFS_TGT \
+            --build=$(build-aux/config.guess)
+
+make
+make DESTDIR=$LFS install
