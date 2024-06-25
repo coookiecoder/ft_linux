a4eeb58df35a7bdd8d4c8dd1f9cbf594066a99d1
Ambroise Bareux <ambroise.bareux@gmail.com>
2024-06-25 03:38:28 +0200

    added libc++ build script

---
+++ script/libc++.sh
@@ -0,0 +1,14 @@
+LFS=/mnt/lfs
+LFS_TGT=$(uname -m)-lfs-linux-gnu
+
+cd /mnt/lfs/sources
+tar -xf gcc-13.2.0.tar.xz
+cd gcc-13.2.0
+
+mkdir -v build
+cd       build
+
+../libstdc++-v3/configure --host=$LFS_TGT --build=$(../config.guess) --prefix=/usr --disable-multilib --disable-nls --disable-libstdcxx-pch --with-gxx-include-dir=/tools/$LFS_TGT/include/c++/13.2.0
+
+make
+make DESTDIR=$LFS install
