de1db2a21ac5c35da26bcfe2e4b95b3363a052b6
Ambroise Bareux <ambroise.bareux@gmail.com>
2024-06-25 05:04:04 +0200

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
-
 wget --input-file=wget-list-sysv --continue --directory-prefix=/mnt/lfs/sources
 mkdir -pv /mnt/lfs/usr /mnt/lfs/var /mnt/lfs/usr/bin /mnt/lfs/usr/lib /mnt/lfs/usr/sbin
 groupadd lfs
 useradd -s /bin/bash -g lfs -m -k /dev/null lfs
 chown -v lfs /mnt/lfs/usr /mnt/lfs/var /mnt/lfs/usr/bin /mnt/lfs/usr/lib /mnt/lfs/usr/sbin
 chown -v lfs /mnt/lfs /mnt/lfs/sources /mnt/lfs/sources/*
+
+for i in bin lib sbin; do
+  ln -sv usr/$i $LFS/$i
+done
+
+case $(uname -m) in
+  x86_64) mkdir -pv $LFS/lib64 ;;
+esac
