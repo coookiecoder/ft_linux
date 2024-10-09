c1cd1d11ee4e67813195622bcceda2865b4a6e0f
Ambroise Bareux <abareux@student.42mulhouse.fr>
2024-10-09 18:45:30 +0200

    updated the build as root to lfs 12.0

--- build_as_root.sh
+++ build_as_root.sh
@@ -24,13 +24,13 @@ mkdir -pv $LFS/run
 
 mount -v --bind /dev $LFS/dev
 
-mount -vt devpts devpts -o gid=5,mode=0620 $LFS/dev/pts
+mount -v --bind /dev/pts $LFS/dev/pts
 mount -vt proc proc $LFS/proc
 mount -vt sysfs sysfs $LFS/sys
 mount -vt tmpfs tmpfs $LFS/run
 
 if [ -h $LFS/dev/shm ]; then
-  install -v -d -m 1777 $LFS$(realpath /dev/shm)
+  mkdir -pv $LFS/$(readlink $LFS/dev/shm)
 else
   mount -vt tmpfs -o nosuid,nodev tmpfs $LFS/dev/shm
 fi
